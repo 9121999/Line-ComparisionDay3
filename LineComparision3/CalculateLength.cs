f302675250e7664648eb930282bfb42a971d5f30
Prerana <dahiwadeprerana@gmail.com>
2022-11-13 14:32:49 +0530

    [prerana]Add:CheckTwoLines

--- LineComparision3/CalculateLength.cs
+++ LineComparision3/CalculateLength.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LineComprasionProblem
+namespace LineComparison
 {
-    internal class CalculateLength
+    public class CalculateLength
     {
-        int x1, y1, x2, y2;
-        public CalculateLength(int x1, int y1, int x2, int y2)
+        int x1, x2, y1, y2;
+        public CalculateLength(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
-            this.y1 = y1;
             this.x2 = x2;
+            this.y1 = y1;
             this.y2 = y2;
         }
         public double Calculate()
         {
-            double cal = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            return cal;
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length:-" + length);
+            return length;
         }
     }
 }
--- LineComparision3/Program.cs
+++ LineComparision3/Program.cs
@@ -1,25 +1,29 @@
 ﻿using System;
-namespace LineComprasionProblem
+
+namespace LineComparison
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            double firstLine, secondLine;
+            CalculateLength calculateLength1 = new CalculateLength(5, 2, 5, 1);
+            double lenght1 = calculateLength1.Calculate();
+            CalculateLength calculateLength2 = new CalculateLength(5, 2, 5, 1);
+            double lenght2 = calculateLength2.Calculate();
 
-            CalculateLength line = new CalculateLength(3, 4, -2, 3);
-            firstLine = line.Calculate();
-            CalculateLength line1 = new CalculateLength(1, 2, 3, 4);
-            secondLine = line1.Calculate();
-
-            if (firstLine.Equals(secondLine))
-            {
-                Console.WriteLine("Calculate lines are Equal");
-            }
+            if (lenght1.Equals(lenght2))
+                Console.WriteLine("Both lines are equal");
             else
+                Console.WriteLine("Both Lines are not equal");
+            if (lenght1.CompareTo(lenght2) == 0)
             {
-                Console.WriteLine(" Calculate  lines are Not equal");
+                Console.WriteLine("Line 1and Line 2 both are equal");
+                return;
             }
+            if (lenght1.CompareTo(lenght2) > 0)
+                Console.WriteLine("Line 1 is greate than Line 2");
+            else
+                Console.WriteLine("Line 2 is greater than Line 1");
         }
     }
 }
